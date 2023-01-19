8eaaf9257e0bd7d365215b6f6b7f4d60165eb29f
burnhambradley20 <116041544+burnhambradley20@users.noreply.github.com>
2023-01-18 21:36:04 -0600

    fixed PORT

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const api = require ('./routes/notes')
 const html = require ('./routes/html')
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
